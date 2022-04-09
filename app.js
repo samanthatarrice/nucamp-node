ae3d5da220d20d2584d05f85536605c6fac8f259
Samantha Tarrice <s.tarrice@gmail.com>
2022-04-09 12:48:13 -0300

    test commit

--- app.js
+++ app.js
@@ -11,6 +11,19 @@ const promotionRouter = require('./routes/promotionRouter');
 const partnerRouter = require('./routes/partnerRouter');
 
 var app = express();
+const mongoose = require('mongoose');
+
+const url = 'mongodb://localhost:27017/nucampsite';
+const connect = mongoose.connect(url, {
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useNewUrlParser: true, 
+    useUnifiedTopology: true
+});
+
+connect.then(() => console.log('Connected correctly to server'), 
+    err => console.log(err)
+);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
--- routes/campsiteRouter.js
+++ routes/campsiteRouter.js
@@ -1,49 +1,75 @@
 const express = require('express');
+const Campsite = require('../models/campsite');
+
 const campsiteRouter = express.Router();
-//Gives us an object that we can use with Express routing methods
 
 campsiteRouter.route('/')
-.all((req, res, next) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  next();
-  //passes control of the routing to the next relevant routing method (below). If the path is to app.post, it will skip app.get and go to that method next.
+.get((req, res, next) => {
+    Campsite.find()
+    .then(campsites => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(campsites);
+    })
+    .catch(err => next(err));
 })
-.get((req, res) => {
-  res.end('Will send all the campsites to you');
-}) //typical approach to add a little functionality at a time
-.post((req, res) => {
-  res.end(`Will add the campsite: ${req.body.name} with description: ${req.body.description}`)
-}) //Post requests typically contain some info within the body of the request
+.post((req, res, next) => {
+    Campsite.create(req.body)
+    .then(campsite => {
+        console.log('Campsite Created ', campsite);
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(campsite);
+    })
+    .catch(err => next(err));
+})
 .put((req, res) => {
-  res.statusCode = 403 //error code used when operation isn't supported
-  res.end('PUT operation not supported on /campsites');
+    res.statusCode = 403;
+    res.end('PUT operation not supported on /campsites');
 })
-.delete((req, res) => {
-  res.end('Deleting all campsites');
-}); //Usually a 'dangerous operation' so you don't want to allow ordinary users to do it. When we study authentication, we will see how we can limit this operation to priviledged users.
-
+.delete((req, res, next) => {
+    Campsite.deleteMany()
+    .then(response => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(response);
+    })
+    .catch(err => next(err));
+});
 
 campsiteRouter.route('/:campsiteId')
-.all((req, res, next) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  next();
-})
-.get((req, res) => {
-  res.end(`Will send details of the campsite: ${req.params.campsiteId} to you`);
+.get((req, res, next) => {
+    Campsite.findById(req.params.campsiteId)
+    .then(campsite => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(campsite);
+    })
+    .catch(err => next(err));
 })
 .post((req, res) => {
-  res.statusCode = 403;
-  res.end(`POST operation not supported on /campsites/${req.params.campsiteId}`);
+    res.statusCode = 403;
+    res.end(`POST operation not supported on /campsites/${req.params.campsiteId}`);
 })
-.put((req, res) => {
-  res.write(`Updating the campsite: ${req.params.campsiteId}\n`);
-  res.end(`Will update the campsite: ${req.body.name} with description: ${req.body.description}`);
+.put((req, res, next) => {
+    Campsite.findByIdAndUpdate(req.params.campsiteId, {
+        $set: req.body
+    }, { new: true })
+    .then(campsite => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(campsite);
+    })
+    .catch(err => next(err));
 })
-.delete((req, res) => {
-  res.end(`Deleting campsite: ${req.params.campsiteId}`);
+.delete((req, res, next) => {
+    Campsite.findByIdAndDelete(req.params.campsiteId)
+    .then(response => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(response);
+    })
+    .catch(err => next(err));
 });
 
 module.exports = campsiteRouter;
-
